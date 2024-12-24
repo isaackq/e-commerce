3640b6ed9d8239443655de490af8abeb686806f4
Hazzembadran <hazemaqbadran@gmail.com>
2024-12-24 10:33:21 +0200

    Improving the code and adding a new test. If the user enters empty values, an error will be returned. I have started using ports and adapters.

--- src/send-message-to-support.spec.ts
+++ src/send-message-to-support.spec.ts
@@ -1,11 +1,14 @@
+import {
+  RequiredError,
+  SendMessageToSupportUseCase,
+} from '@application/usecase/send-message-to-support.usecase';
 import type { Message } from '@domain/entities/Message';
+import type { MessageRepositoryInterface } from '@domain/ports/message.repository.interface';
 import { describe, expect, test } from '@jest/globals';
-
-import { SendMessageToSupportUseCase } from './application/usecase/send-message-to-support.usecase';
 
 describe('Feature: Send message To Support', () => {
   describe('Scenario: Successfuly send message', () => {
-    test('Role: ', () => {
+    test('Role: As a user, I can sent message to support on the platform', () => {
       whenMessageToSupport({
         title: 'Name',
         subject: 'Hazem Hazem',
@@ -16,15 +19,52 @@ describe('Feature: Send message To Support', () => {
       });
     });
   });
+
+  describe('Scenario: Prevent user send message with empty ', () => {
+    test('Role: As a user, I cannot register with empty tilte or subject or both ', () => {
+      whenMessageToSupport({
+        title: '',
+        subject: '',
+      });
+      thenUserConnotSendMessageToSupport(RequiredError);
+    });
+  });
 });
 
+export class InMemoryMessageRepository implements MessageRepositoryInterface {
+  messages: Message[] = [];
+  save(registrationCommand: Message): Message {
+    this.messages.push(registrationCommand);
+    return registrationCommand;
+  }
+  findOne(): Message {
+    return this.messages[0];
+  }
+}
+
+let throwError: Error;
+
+let inMemoryMessageRepository = new InMemoryMessageRepository();
+
 let mssageToSupportUseCase: SendMessageToSupportUseCase =
-  new SendMessageToSupportUseCase();
+  new SendMessageToSupportUseCase(inMemoryMessageRepository);
 
 function whenMessageToSupport(messageToSupportCommand: Message) {
-  mssageToSupportUseCase.execute(messageToSupportCommand);
+  try {
+    mssageToSupportUseCase.execute(messageToSupportCommand);
+  } catch (error) {
+    if (error instanceof Error) {
+      throwError = error;
+    } else {
+      console.log(error);
+    }
+  }
 }
 
 function thenMessageToSupport(expectedMessage: Message) {
-  expect(expectedMessage).toEqual(mssageToSupportUseCase.message);
+  expect(expectedMessage).toEqual(inMemoryMessageRepository.findOne());
+}
+
+function thenUserConnotSendMessageToSupport(expectedError: new () => Error) {
+  expect(throwError).toBeInstanceOf(expectedError);
 }
