c78280883b56443d67d51078530a707f79a3505c
Hazzembadran <hazemaqbadran@gmail.com>
2024-12-24 11:31:35 +0200

    The code includes the definition and usage of a Fixture

--- src/send-message-to-support.spec.ts
+++ src/send-message-to-support.spec.ts
@@ -4,16 +4,21 @@ import {
 } from '@application/usecase/send-message-to-support.usecase';
 import type { Message } from '@domain/entities/Message';
 import { InMemoryMessageRepository } from '@infrastructure/repositories/in-memory.message.repository';
-import { describe, expect, test } from '@jest/globals';
+import { beforeEach, describe, expect, test } from '@jest/globals';
 
 describe('Feature: Send message To Support', () => {
+  let fixture: Fixture;
+  beforeEach(() => {
+    fixture = createFixture();
+  });
+
   describe('Scenario: Successfuly send message', () => {
     test('Role: As a user, I can sent message to support on the platform', () => {
-      whenMessageToSupport({
+      fixture.whenMessageToSupport({
         title: 'Name',
         subject: 'Hazem Hazem',
       });
-      thenMessageToSupport({
+      fixture.thenMessageToSupport({
         title: 'Name',
         subject: 'Hazem Hazem',
       });
@@ -22,38 +27,44 @@ describe('Feature: Send message To Support', () => {
 
   describe('Scenario: Prevent user send message with empty ', () => {
     test('Role: As a user, I cannot register with empty tilte or subject or both ', () => {
-      whenMessageToSupport({
+      fixture.whenMessageToSupport({
         title: '',
         subject: '',
       });
-      thenUserConnotSendMessageToSupport(RequiredError);
+      fixture.thenUserConnotSendMessageToSupport(RequiredError);
     });
   });
 });
 
-let throwError: Error;
+function createFixture() {
+  let throwError: Error;
 
-let inMemoryMessageRepository = new InMemoryMessageRepository();
+  let inMemoryMessageRepository = new InMemoryMessageRepository();
 
-let mssageToSupportUseCase: SendMessageToSupportUseCase =
-  new SendMessageToSupportUseCase(inMemoryMessageRepository);
+  let mssageToSupportUseCase: SendMessageToSupportUseCase =
+    new SendMessageToSupportUseCase(inMemoryMessageRepository);
 
-function whenMessageToSupport(messageToSupportCommand: Message) {
-  try {
-    mssageToSupportUseCase.execute(messageToSupportCommand);
-  } catch (error) {
-    if (error instanceof Error) {
-      throwError = error;
-    } else {
-      console.log(error);
-    }
-  }
+  return {
+    whenMessageToSupport(messageToSupportCommand: Message) {
+      try {
+        mssageToSupportUseCase.execute(messageToSupportCommand);
+      } catch (error) {
+        if (error instanceof Error) {
+          throwError = error;
+        } else {
+          console.log(error);
+        }
+      }
+    },
+
+    thenMessageToSupport(expectedMessage: Message) {
+      expect(expectedMessage).toEqual(inMemoryMessageRepository.findOne());
+    },
+
+    thenUserConnotSendMessageToSupport(expectedError: new () => Error) {
+      expect(throwError).toBeInstanceOf(expectedError);
+    },
+  };
 }
 
-function thenMessageToSupport(expectedMessage: Message) {
-  expect(expectedMessage).toEqual(inMemoryMessageRepository.findOne());
-}
-
-function thenUserConnotSendMessageToSupport(expectedError: new () => Error) {
-  expect(throwError).toBeInstanceOf(expectedError);
-}
+type Fixture = ReturnType<typeof createFixture>;
